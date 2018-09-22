3f73b57a95a0cfb0317f54319ae3a3fefe421225
iamstenman <39154602+itwysgsl@users.noreply.github.com>
2018-09-22 09:27:18 +0900

    Added support of GET requests

--- rpcd.py
+++ rpcd.py
@@ -6,11 +6,37 @@ Usage::
 from aiorpcx import ClientSession
 from server.controller import Controller
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import parse
 from os import environ
 import asyncio
 import json
 import re
 
+port = int(environ.get('RPC_PORT', 7403))
+rpc_port = int(environ.get('RPC_PORT', 8000))
+dead_response = {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": None}
+allowed = [
+    'blockchain.address.get_balance',
+    'blockchain.address.get_history',
+    'blockchain.address.get_mempool',
+    'blockchain.address.listunspent',
+    'blockchain.address.get_utxo',
+    'blockchain.address.get_utxo_amount',
+    'blockchain.address.subscribe',
+    'blockchain.address.info',
+    'blockchain.address.history',
+    'blockchain.block.get_header',
+    'blockchain.block.get_header_range',
+    'blockchain.block.get_header_info',
+    'blockchain.estimatesmartfee',
+    'blockchain.headers.subscribe',
+    'blockchain.relayfee',
+    'blockchain.transaction.broadcast',
+    'blockchain.transaction.get',
+    'blockchain.transaction.get_verbose',
+    'blockchain.transaction.get_merkle',
+    'getinfo'
+]
 
 def handle_rpc(raw_data):
     result = {
@@ -91,61 +117,50 @@ class RpcServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
+
+    async def send_request(self, request_self, method, params, rid):
+        client_port = port
+        if method in ["getinfo"]:
+            client_port = rpc_port
+            
+        async with ClientSession('localhost', client_port) as session:
+            try:
+                response = await session.send_request(method, params, timeout=15)
+            except Exception as e:
+                response = e
+
+        request_self._set_response()
+        request_self.wfile.write(json.dumps(create_rpc(response, rid)).encode('utf-8'))
+
+    def do_GET(self):
+        data = parse.parse_qs(parse.urlparse(self.path).query)
+
+        rid = data["id"] if ("id" in data) else "electrumx"
+        method = data["method"][0] if ("method" in data) else []
+        params = data["params[]"] if ("params[]" in data) else []
+        loop = asyncio.get_event_loop()
+
+        try:
+            loop.run_until_complete(self.send_request(self, method, params, rid))
+        except OSError:
+            print('cannot connect - is ElectrumX catching up, not running, or '
+                  f'is {port} the wrong RPC port?')
+        except Exception as e:
+            print(f'error making request: {e}')
         
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-
-        response = {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": None}
         data = handle_rpc(post_data.decode('utf-8'))
-        rpc_port = int(environ.get('RPC_PORT', 8000))
-        port = int(environ.get('RPC_PORT', 7403))
 
         if "error" not in data:
-
-            method = data["method"]
-            params = data["params"]
-
-            allowed = [
-                'blockchain.address.get_balance',
-                'blockchain.address.get_history',
-                'blockchain.address.get_mempool',
-                'blockchain.address.listunspent',
-                'blockchain.address.get_utxo',
-                'blockchain.address.get_utxo_amount',
-                'blockchain.address.subscribe',
-                'blockchain.address.info',
-                'blockchain.address.history',
-                'blockchain.block.get_header',
-                'blockchain.block.get_header_range',
-                'blockchain.block.get_header_info',
-                'blockchain.estimatefee',
-                'blockchain.headers.subscribe',
-                'blockchain.relayfee',
-                'blockchain.transaction.broadcast',
-                'blockchain.transaction.get',
-                'blockchain.transaction.get_verbose',
-                'blockchain.transaction.get_merkle',
-                'getinfo'
-            ]
-
-            async def send_request():
-                client_port = port
-                if method in ["getinfo"]:
-                    client_port = rpc_port
-                    
-                async with ClientSession('localhost', client_port) as session:
-                    try:
-                        response = await session.send_request(method, params, timeout=15)
-                    except Exception as e:
-                        response = e
-
-                self._set_response()
-                self.wfile.write(json.dumps(create_rpc(response, data["id"])).encode('utf-8'))
-
+            rid = data["id"] if ("id" in data) else "electrumx"
+            method = data["method"] if ("method" in data) else []
+            params = data["params"] if ("params" in data) else []
             loop = asyncio.get_event_loop()
+            
             try:
-                loop.run_until_complete(send_request())
+                loop.run_until_complete(self.send_request(self, method, params, rid))
             except OSError:
                 print('cannot connect - is ElectrumX catching up, not running, or '
                       f'is {port} the wrong RPC port?')
@@ -154,10 +169,10 @@ class RpcServer(BaseHTTPRequestHandler):
 
         else:
             self._set_response()
-            self.wfile.write(json.dumps(response, indent=4, sort_keys=True).encode('utf-8'))
+            self.wfile.write(json.dumps(dead_response, indent=4, sort_keys=True).encode('utf-8'))
 
 
-def run(server_class=HTTPServer, handler_class=RpcServer, port=4321):
+def run(server_class=HTTPServer, handler_class=RpcServer, port=1234):
     server_address = ('', port)
     rpcd = server_class(server_address, handler_class)
     print('Starting rpcd on port {}...\n'.format(port))
